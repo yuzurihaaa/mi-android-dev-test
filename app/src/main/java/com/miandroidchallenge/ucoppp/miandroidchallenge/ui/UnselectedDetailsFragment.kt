969340438074deca0ae0ac963ee27e094fddf17c
ucoppp <yusuf@primekeeper.my>
2018-05-20 16:48:55 +0800

    tablet ui

---
+++ app/src/main/java/com/miandroidchallenge/ucoppp/miandroidchallenge/ui/UnselectedDetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.miandroidchallenge.ucoppp.miandroidchallenge.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.miandroidchallenge.ucoppp.miandroidchallenge.R
+
+class UnselectedDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): UnselectedDetailsFragment = UnselectedDetailsFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_unselected_details, container, false)
+    }
+}
--- app/src/main/java/com/miandroidchallenge/ucoppp/miandroidchallenge/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/com/miandroidchallenge/ucoppp/miandroidchallenge/ui/mainactivity/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.miandroidchallenge.ucoppp.miandroidchallenge.ui.mainactivity
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.miandroidchallenge.ucoppp.miandroidchallenge.R
 import com.miandroidchallenge.ucoppp.miandroidchallenge.models.Deliveries
+import com.miandroidchallenge.ucoppp.miandroidchallenge.ui.UnselectedDetailsFragment
 import com.miandroidchallenge.ucoppp.miandroidchallenge.ui.deliverydetailsfragment.DeliveryDetailsFragment
 import com.miandroidchallenge.ucoppp.miandroidchallenge.ui.deliverylistfragment.DeliveryFragment
 import com.miandroidchallenge.ucoppp.miandroidchallenge.ui.deliverylistfragment.interfaces.OnDeliveryCallback
@@ -11,19 +13,69 @@ import com.miandroidchallenge.ucoppp.miandroidchallenge.ui.deliverylistfragment.
 
 class MainActivity : AppCompatActivity(), OnDeliveryCallback {
 
+    val DELIVERY_STATE = "delivery_state"
+
+    var tabletSize: Boolean = false
+
+    var screenChangingId: Int = R.id.fragment_holder
+
+    var deliveries: Deliveries? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_holder, DeliveryFragment.newInstance())
-                .commit()
+        tabletSize = resources.getBoolean(R.bool.isTablet)
+
+        if (tabletSize) {
+
+            screenChangingId = R.id.fragment_details_holder
+
+            replaceFragment(R.id.fragment_list_holder, DeliveryFragment.newInstance())
+
+            if (savedInstanceState != null) {
+
+                val delivery = savedInstanceState.getParcelable<Deliveries>(DELIVERY_STATE)
+
+                replaceFragment(screenChangingId, DeliveryDetailsFragment.newInstance(delivery))
+            } else {
+
+                replaceFragment(screenChangingId, UnselectedDetailsFragment.newInstance())
+            }
+
+        } else {
+
+            screenChangingId = R.id.fragment_holder
+
+            replaceFragment(screenChangingId, DeliveryFragment.newInstance())
+        }
 
     }
 
     override fun onClickDelivery(delivery: Deliveries) {
+        deliveries = delivery
+        replaceFragmentWithPop(screenChangingId, DeliveryDetailsFragment.newInstance(delivery))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(DELIVERY_STATE, deliveries)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        deliveries = savedInstanceState?.getParcelable(DELIVERY_STATE)
+    }
+
+    private fun replaceFragment(fragmentId: Int, fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_holder, DeliveryDetailsFragment.newInstance(delivery))
+                .replace(fragmentId, fragment)
+                .commit()
+    }
+
+    private fun replaceFragmentWithPop(fragmentId: Int, fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(fragmentId, fragment)
                 .addToBackStack(null)
                 .commit()
     }
